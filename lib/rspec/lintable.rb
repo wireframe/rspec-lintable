c4341d00a36be0189ff54261da77bedc3e3f4490
Ryan Sonnek <ryan@codecrate.com>
2014-03-01 04:38:37 +0100

    initial checkin

---
+++ lib/rspec/lintable.rb
@@ -0,0 +1,35 @@
+require "rspec/lintable/version"
+require 'jshintrb'
+require 'json'
+
+# ensure that response body is valid "lintable" javascript.
+# this matcher should *not* be used for negative assertions (ie: should_not be_lintable)
+#
+# example usage:
+#   describe "#show" do
+#     before { get :show, format: :js }
+#     it { should be_lintable }
+#   end
+#
+# see https://gist.github.com/wireframe/1042920
+RSpec::Matchers.define :be_lintable do |ability|
+  match do |controller|
+    @content = controller.response.body
+    @errors = Jshintrb.lint(@content, jshint_options, jshint_globals)
+    @errors.empty?
+  end
+
+  failure_message_for_should do |actual|
+    "expected response javascript to be lintable.\nErrors: #{@errors.inspect}\nContent: #{@content}"
+  end
+
+  private
+
+  def jshint_options
+    @jshint_options ||= JSON.load(File.read(Rails.root.join('.jshintrc')))
+  end
+
+  def jshint_globals
+    @globals ||= jshint_options.delete('globals').keys
+  end
+end
---
+++ lib/rspec/lintable/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module Lintable
+    VERSION = "1.0.0"
+  end
+end
